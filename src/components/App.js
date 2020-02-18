5bec3d24f2708832611fe3cfff82609ba31b417e
David Montesdeoca <backpackerhh@gmail.com>
2020-02-18 11:59:18 +0100

    Add info about site under construction

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hi from App!</div>;
+  return (
+    <div className="ui center aligned container grid" style={{ marginTop: "5%", fontSize: "21px" }}>
+      <div className="row">
+        <picture>
+          <img
+            src="/images/davidmontesdeoca-200.png"
+            alt="David Montesdeoca"
+            className="ui centered circular image"
+          />
+        </picture>
+      </div>
+
+      <div className="row">
+        <header>
+          <h1 className="ui huge header" data-testid="header">
+            Hi! I'm David Montesdeoca
+          </h1>
+          <p>
+            I'm a software developer from Las Palmas de Gran Canaria, Spain and currently living in
+            Madrid.
+          </p>
+        </header>
+      </div>
+
+      <div className="row">
+        <div className="ui massive message">
+          <i className="info circle icon"></i>
+          New site is coming soon...
+        </div>
+      </div>
+
+      <div className="row">
+        <p>
+          Right now, if you want to know more about me, please check{" "}
+          <a href="https://www.linkedin.com/in/backpackerhh">my LinkedIn profile</a>.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -3,8 +3,12 @@ import { render } from "@testing-library/react";
 
 import App from "./../App";
 
-it("renders expected greeting", () => {
-  const { getByText } = render(<App />);
+it("renders expected content", () => {
+  const { getByTestId, getByText } = render(<App />);
 
-  expect(getByText("Hi from App!")).toBeInTheDocument();
+  expect(getByTestId("header")).toHaveTextContent("David Montesdeoca");
+  expect(getByText("my LinkedIn profile").closest("a")).toHaveAttribute(
+    "href",
+    "https://www.linkedin.com/in/backpackerhh"
+  );
 });
