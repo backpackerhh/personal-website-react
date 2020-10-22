4bee5bbdcf4cc41151b7e73b958cf7e645b3c2d1
David Montesdeoca <backpackerhh@gmail.com>
2020-10-22 19:54:26 +0200

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,38 @@ import withSuspense from "../utils/withSuspense";
 
 const Footer = () => {
   const { t } = useTranslation("footer");
+  const year = new Date().getFullYear();
 
-  return "Footer";
+  return (
+    <>
+      <div className="ui divider"></div>
+
+      <div className="ui computer reversed center aligned three column grid">
+        <div className="middle aligned row">
+          <div className="sixteen wide mobile sixteen wide tablet four wide computer column">
+            <div className="ui tiny label">
+              <p>
+                <i className="github alternate large icon"></i>
+                {t("source_code")}
+                <a href="https://github.com/backpackerhh/personal-website-react">GitHub</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="sixteen wide mobile sixteen wide tablet eight wide computer column">
+            <p id="license">
+              <small>
+                <span id="copyleft">&copy;</span> {year} David Montesdeoca. {t("under_license")}
+                <a href="http://choosealicense.com/licenses/gpl-3.0/">GNU GPLv3</a>
+              </small>
+            </p>
+          </div>
+
+          <div className="four wide computer only column"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default withSuspense(Footer);
