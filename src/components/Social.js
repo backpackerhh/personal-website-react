7fb9c2ccc9d5710dd448900074628da508e140b0
David Montesdeoca <backpackerhh@gmail.com>
2023-04-05 09:23:08 +0200

    Remove Twitter link

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,14 +4,13 @@ import SocialItem from "./SocialItem";
 
 const Social = () => {
   return (
-    <div className="ui five column stackable grid">
+    <div className="ui four column stackable grid">
       <div className="row">
         <SocialItem
           text="LinkedIn"
           icon="linkedin big"
           url="https://linkedin.com/in/backpackerhh"
         />
-        <SocialItem text="Twitter" icon="twitter big" url="https://twitter.com/backpackerhh" />
         <SocialItem text="GitHub" icon="github big" url="https://github.com/backpackerhh" />
         <SocialItem
           text="Stack Overflow"
